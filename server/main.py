b0d3a2fa5f6debedb7c908468a4bd9d814dffc85
Guayben <clement.calujek@gmail.com>
2024-04-03 11:30:59 +0200

    PROGRESS IG

--- server/main.py
+++ server/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Utilisez render_template pour servir votre fichier HTML
-    return render_template('index.html')
+    return render_template('IG_MESCnn.html')
 
 
 
