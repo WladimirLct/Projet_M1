c1f024129f58d9d23beb9e55a2814b254ab8fa5c
Antoine MIGNIEN <72608526+AntoineMIGNIEN@users.noreply.github.com>
2024-04-04 11:25:15 +0200

    push

--- server/main.py
+++ server/main.py
@@ -10,7 +10,8 @@ def home():
 
 @app.route('/mescnn')
 def run_mescnn():
-    mescnn_function()
+    path = "/home/antoine/Documents/GitHub/MESCnn/Data/Dataset/WSI/C2321120-1-A-PAS(1).svs"
+    mescnn_function(path)
     return 'MESCnn run complete!'
 
 #TEST pour les fetchs 
