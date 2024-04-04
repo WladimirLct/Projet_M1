ae75b0b1826f0f60daa1c5aba1259317a06de8b9
WladimirLct <wlad.lucet@gmail.com>
2024-04-04 11:23:31 +0200

    add file upload

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, render_template, request, jsonify
+from werkzeug.utils import secure_filename
 from run_wsi_tif import mescnn_function
 
 app = Flask(__name__)
@@ -13,12 +15,28 @@ def run_mescnn():
     mescnn_function()
     return 'MESCnn run complete!'
 
-#TEST pour les fetchs 
-@app.route('/test', methods=['POST'])
-def test():
-    test = request.json['test']
-    return jsonify({'test': test})
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    if 'file' not in request.files:
+        return 'No file part', 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file', 400
+    
+    if file:
+        # Check if there is already a file in the folder
+        files = os.listdir('./current-file/')
+        # Remove the file(s) in the folder
+        for f in files:
+            os.remove('./current-file/' + f)
 
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('./current-file/', filename))
+        # process the file here
+        return 'File uploaded and processed successfully'
+    return 'Error processing file', 400
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
-    
