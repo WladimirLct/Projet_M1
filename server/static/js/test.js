94fcd7892b2b3e1f635b061c9e89ccc31b184a49
WladimirLct <wlad.lucet@gmail.com>
2024-04-05 10:30:59 +0200

    Revert "Merge branch 'HomePage'"
    This reverts commit 23db59e00117ce199e4f41be8c3f693eb7370e7e, reversing
    changes made to 03cce26f202e5f72326e7379e248c6ab95cb9466.

--- server/static/js/test.js
+++ server/static/js/test.js
@@ -1,29 +1,3 @@
-//JS pour la loading bar
-document.getElementById('analyzeButton').addEventListener('click', startAnalyzing);
-
-function startAnalyzing() {
-    const loadingBarContainer = document.getElementById('loadingBarContainer');
-    const loadingBar = document.getElementById('loadingBar');
-
-    loadingBarContainer.classList.remove('hidden'); // Show loading bar container
-    loadingBar.style.width = '0%'; // Reset width
-
-    let progress = 0;
-    const interval = setInterval(() => {
-        progress += 1; // Increment progress more slowly
-        loadingBar.style.width = `${progress}%`;
-
-        if (progress >= 100) {
-            clearInterval(interval);
-            setTimeout(() => {
-                loadingBarContainer.classList.add('hidden');
-                loadingBar.style.width = '0%';
-            }, 2000); // Keep the loading bar visible a bit longer after reaching 100%
-        }
-    }, 20); // Adjust speed for a more gradual progress
-}
-
-
 //JS TEST/EX POUR LES FETCHS
 
 function click() {
