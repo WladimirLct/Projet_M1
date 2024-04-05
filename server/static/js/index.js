b8c858fe6452502cd405c013dea3f712cc9da5af
WladimirLct <wlad.lucet@gmail.com>
2024-04-05 15:10:35 +0200

    Progress bar fix

--- server/static/js/index.js
+++ server/static/js/index.js
@@ -1,32 +1,20 @@
 const socket = io(); //socketio connection to server//
 
+const loadingBarContainer = document.getElementById('loadingBarContainer');
+const loadingBar = document.getElementById('loadingBar');
+
 let form = document.getElementById('dropzone-form');
 
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    // Set the cursor to loading
+    document.body.style.cursor = 'wait';
+
     let formData = new FormData();
     let fileField = document.querySelector('input[type="file"]');
 
     formData.append('file', fileField.files[0]);
-
-    fetch('/analyze', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        window.location.replace('/waiting');
-    })
-    .catch(error => console.error('Error:', error));
-});
-
-//JS for the loading bar
-document.getElementById('analyze-btn').addEventListener('click', startAnalyzing);
-
-function startAnalyzing() {
-    const loadingBarContainer = document.getElementById('loadingBarContainer');
-    const loadingBar = document.getElementById('loadingBar');
 
     loadingBarContainer.classList.remove('hidden'); // Show loading bar container
     loadingBar.style.width = '0%'; // Reset width
@@ -38,10 +26,21 @@ function startAnalyzing() {
 
         if (progress >= 100) {
             clearInterval(interval);
-            setTimeout(() => {
-                loadingBarContainer.classList.add('hidden');
-                loadingBar.style.width = '0%';
-            }, 2000); // Keep the loading bar visible a bit longer after reaching 100%
         }
-    }, 20); // Adjust speed for a more gradual progress
-}
+    }, 50); // Adjust speed for a more gradual progress
+
+    fetch('/analyze', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.text())
+    .then(data => {
+        loadingBar.style.width = '100%';
+        // Redirect to /waiting after 3 seconds
+        setTimeout(() => {
+            window.location.href = '/waiting';
+        }, 3000);
+
+    })
+    .catch(error => console.error('Error:', error));
+});
